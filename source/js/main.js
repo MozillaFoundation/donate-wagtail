db5a7c5e4cddce8c8c9edb770cd9f3edb0e3317d
nicklee <nick@nicklee.co.uk>
2019-07-11 16:29:31 +0100

    Primary nav js and styling
    Mobile menu updated to menu toggle - it’s no longer just used for mobile

--- source/js/main.js
+++ source/js/main.js
@@ -1,35 +1,21 @@
 import 'babel-polyfill';
 
-import MobileMenu from './components/mobile-menu';
-import MobileSubMenu from './components/mobile-sub-menu';
+import MenuToggle from './components/menu-toggle';
 
 // Open the mobile menu callback
-function openMobileMenu() {
-    document.querySelector('body').classList.add('no-scroll');
-    document.querySelector('[data-mobile-menu]').classList.add('is-visible');
+function openMenu() {
+    document.querySelector('[data-primary-nav]').classList.add('is-visible');
 }
 
 // Close the mobile menu callback.
-function closeMobileMenu() {
-    document.querySelector('body').classList.remove('no-scroll');
-    document.querySelector('[data-mobile-menu]').classList.remove('is-visible');
+function closeMenu() {
+    document.querySelector('[data-primary-nav]').classList.remove('is-visible');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    for (const mobilemenu of document.querySelectorAll(MobileMenu.selector())) {
-        new MobileMenu(mobilemenu, openMobileMenu, closeMobileMenu);
-    }
-
-    for (const mobilesubmenu of document.querySelectorAll(MobileSubMenu.selector())) {
-        new MobileSubMenu(mobilesubmenu);
-    }
-
-    // Toggle subnav visibility
-    for (const subnavBack of document.querySelectorAll('[data-subnav-back]')) {
-        subnavBack.addEventListener('click', () => {
-            subnavBack.parentNode.classList.remove('is-visible');
-        });
+    for (const menutoggle of document.querySelectorAll(MenuToggle.selector())) {
+        new MenuToggle(menutoggle, openMenu, closeMenu);
     }
 
 });
