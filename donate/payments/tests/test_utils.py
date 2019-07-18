577f4ede24c08f339600feb523a1441c1beb2557
Samir Shah <solaris.smoke@gmail.com>
2019-07-18 14:29:53 +0300

    Collect first and last name separately on details form.

--- donate/payments/tests/test_utils.py
+++ donate/payments/tests/test_utils.py
@@ -9,10 +9,12 @@ class UtilsTestCase(TestCase):
 
     def test_freeze_personal_detail_stringifies_decimal_amount(self):
         data = {
-            'name': 'Alice',
+            'first_name': 'Alice',
+            'last_name': 'Bob',
             'amount': Decimal(50)
         }
         self.assertEqual(freeze_personal_details_for_session(data), {
-            'name': 'Alice',
+            'first_name': 'Alice',
+            'last_name': 'Bob',
             'amount': '50'
         })
