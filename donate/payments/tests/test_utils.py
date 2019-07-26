a451630a0bfcdd647450d41adcd4a819bc086e2c
Samir Shah <solaris.smoke@gmail.com>
2019-07-26 09:54:29 +0300

    Add currency detection functionality.

--- donate/payments/tests/test_utils.py
+++ donate/payments/tests/test_utils.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 from django.test import TestCase
 
-from ..utils import freeze_transaction_details_for_session
+from ..utils import freeze_transaction_details_for_session, get_default_currency
 
 
 class UtilsTestCase(TestCase):
@@ -18,3 +18,30 @@ class UtilsTestCase(TestCase):
             'last_name': 'Bob',
             'amount': '50'
         })
+
+    def test_get_default_currency_uses_map(self):
+        self.assertEqual(
+            get_default_currency('en-AU;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5'),
+            'aud'
+        )
+        self.assertEqual(
+            get_default_currency('en-CA;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5'),
+            'cad'
+        )
+
+    def test_get_default_currency_next_language_pre(self):
+        # First preference is a bogus language. Second one is korean.
+        self.assertEqual(
+            get_default_currency('fo-FO;q=0.9, ko;q=0.8'),
+            'krw'
+        )
+
+    def test_get_default_currency_falls_back_to_base_language(self):
+        self.assertEqual(
+            get_default_currency('es-GG;q=0.9'),
+            'eur'
+        )
+
+    def test_get_default_currency_fallback_to_usd(self):
+        self.assertEqual(get_default_currency(''), 'usd')
+        self.assertEqual(get_default_currency('foo'), 'usd')
