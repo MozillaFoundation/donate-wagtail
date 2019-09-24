15c96286c896ec29ebcc2fd3d080686c990eb5b2
Théo Chevalier <theo.chevalier11@gmail.com>
2019-09-24 17:12:52 +0200

    Django i18n polishing

--- donate/payments/forms.py
+++ donate/payments/forms.py
@@ -68,6 +68,6 @@ class NewsletterSignupForm(forms.Form):
     email = forms.EmailField()
     privacy = forms.BooleanField(
         label=mark_safe(_(
-            "I'm okay with Mozilla handling my info as explained in this <a %(attrs)s>Privacy Notice</a>."
+            "I’m okay with Mozilla handling my info as explained in this <a %(attrs)s>Privacy Notice</a>."
         ) % {'attrs': 'href="https://www.mozilla.org/privacy/websites/"'})
     )
