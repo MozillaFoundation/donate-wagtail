814892dbd8c9180929ac48861b0b51805d9ff0e9
Christopher DeCairos <cade@mozillafoundation.org>
2019-10-01 15:03:23 -0400

    try to compile messages in post_compile

--- release-steps.sh
+++ release-steps.sh
@@ -2,8 +2,8 @@
 
 # Django Migrations
 python manage.py migrate --no-input
-
-PATH=$HOME/gettext/bin:$PATH
-LD_LIBRARY_PATH=$HOME/gettext/lib:$LD_LIBRARY_PATH
-
-python manage.py compilemessages
+#
+#PATH=$HOME/gettext/bin:$PATH
+#LD_LIBRARY_PATH=$HOME/gettext/lib:$LD_LIBRARY_PATH
+#
+#python manage.py compilemessages
