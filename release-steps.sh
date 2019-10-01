4561da0ee96a40ff528d23b2dd15987d2e244f80
Lucie Daeye <lucied@mozillafoundation.org>
2019-10-01 16:52:20 +0200

    debug time \o/

--- release-steps.sh
+++ release-steps.sh
@@ -2,4 +2,10 @@
 
 # Django Migrations
 python manage.py migrate --no-input
+<<<<<<< HEAD
+=======
+PATH=$PATH:/app/bin/
+echo $PATH
+ls /app/bin
+>>>>>>> debug time \o/
 python manage.py compilemessages
