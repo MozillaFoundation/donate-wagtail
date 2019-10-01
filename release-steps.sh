b822bd72885948ef9fdd1b9a30228aadc52cce02
Christopher DeCairos <cade@mozillafoundation.org>
2019-10-01 15:08:43 -0400

    bit of cleanup

--- release-steps.sh
+++ release-steps.sh
@@ -2,8 +2,3 @@
 
 # Django Migrations
 python manage.py migrate --no-input
-#
-#PATH=$HOME/gettext/bin:$PATH
-#LD_LIBRARY_PATH=$HOME/gettext/lib:$LD_LIBRARY_PATH
-#
-#python manage.py compilemessages
