d3201853a76fa6ea0eafd84c2f1b9815bef7324b
Samir Shah <solaris.smoke@gmail.com>
2019-10-02 15:41:39 +0300

    Add support for a handful of language codes not recognised by Django and Babel.

--- donate/core/constants.py
+++ donate/core/constants.py
@@ -1,5 +1,10 @@
 # Mapping of locales that Babel doesn't recognise to fallbacks that it does recognise
 
 LOCALE_MAP = {
-    'es-xl': 'es'
+    'ach': 'en',
+    'es-XL': 'es',
+    'lg': 'en',
+    'lo': 'en',
+    'ms': 'en',
+    'uk': 'en',
 }
