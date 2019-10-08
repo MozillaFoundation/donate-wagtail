43f94d604aad6f48376baa76efa9d42f2e66d08f
nicklee <nick@nicklee.co.uk>
2019-10-08 13:27:49 +0100

    Share functionality

---
+++ source/js/components/copy-url.js
@@ -0,0 +1,32 @@
+class CopyURL {
+  static selector() {
+    return "[data-copy-link]";
+  }
+
+  constructor(node) {
+    this.button = node;
+    this.input = this.button.querySelector("[data-copy-value]");
+    this.bindEvents();
+  }
+
+  copyText() {
+    this.input.select();
+    this.input.setSelectionRange(0, 99999); /*For mobile devices*/
+    document.execCommand("copy");
+    alert("Copied the text: " + this.input.value);
+  }
+
+  updateButton() {
+    this.button.classList.add("copied");
+  }
+
+  bindEvents() {
+    this.button.addEventListener("click", e => {
+      e.preventDefault();
+      this.copyText();
+      this.updateButton();
+    });
+  }
+}
+
+export default CopyURL;
