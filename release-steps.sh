7b5539f7fd110e1027806f6812b2724c9fe5885a
Lucie Daeye <lucied@mozillafoundation.org>
2019-10-31 14:53:27 +0100

    create .ssh dir

--- release-steps.sh
+++ release-steps.sh
@@ -4,4 +4,5 @@
 python manage.py migrate --no-input
 
 # Configuring ssh key to be able to push content that needs to be translated with Pontoon to a certain repo (`WAGTAILLOCALIZE_PONTOON_GIT_URL` env variable).
+mkdir /app/.ssh/
 echo $SSH_KEY > /app/.ssh/id_rsa
