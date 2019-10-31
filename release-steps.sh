55fd893f985465320a48e386ae11ff44c3515528
Lucie Daeye <lucied@mozillafoundation.org>
2019-10-31 14:33:40 +0100

    Create the id_rsa file during the release steps

--- release-steps.sh
+++ release-steps.sh
@@ -2,3 +2,6 @@
 
 # Django Migrations
 python manage.py migrate --no-input
+
+# Configuring ssh key to be able to push content that needs to be translated with Pontoon to a certain repo (`WAGTAILLOCALIZE_PONTOON_GIT_URL` env variable).
+echo $SSH_KEY > /app/.ssh/id_rsa
