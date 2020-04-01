63c82e3193b57202bf41b4acb7b1a66c0001c7af
Christopher DeCairos <cade@mozillafoundation.org>
2020-04-01 14:45:58 -0400

    Provide PayPal with a custom DisplayName value using the Frontend environment configuration endpoint.

--- donate/settings/thunderbird.py
+++ donate/settings/thunderbird.py
@@ -1,6 +1,13 @@
-from .environment import app
+from .environment import app, env
 
 
 class ThunderbirdOverrides(object):
     INSTALLED_APPS = ['donate.thunderbird']
     TEMPLATES_DIR = [app('thunderbird/templates')]
+    FRONTEND = {
+        'RELEASE_VERSION': env('HEROKU_RELEASE_VERSION'),
+        'SENTRY_DSN': env('SENTRY_DSN'),
+        'SENTRY_ENVIRONMENT': env('SENTRY_ENVIRONMENT'),
+        'BRAINTREE_MERCHANT_ACCOUNTS': env('BRAINTREE_MERCHANT_ACCOUNTS'),
+        'PAYPAL_DISPLAY_NAME': 'MZLA Technologies'
+    }
