7e5f4cc8d8c979956d2345b6e1476f116f1671fe
Daniel Miranda <daniel@mozillafoundation.org>
2021-03-26 14:59:25 -0700

    updated html to show error message below other amount input for one time/ monthly donation. Also updated validation.js to account for both inputs

--- source/js/components/validation.js
+++ source/js/components/validation.js
@@ -1,22 +1,42 @@
-// Handler for "other amount" input
+// Handler for "other amount" inputs
 function otherAmountInputValidation() {
-  const otherAmountInput = document.querySelector(
+  const oneTimeOtherAmountInput = document.querySelector(
     ".one-time-amount-other-input"
   );
-  const otherAmountErrorMessage = document.querySelector(".error-message");
-  otherAmountInput.addEventListener("blur", (e) => {
+  const monthlyOtherAmountInput = document.querySelector(
+    ".monthly-amount-other-input"
+  );
+  const oneTimeOtherAmountErrorMessage = document.querySelector(
+    ".error-message__one-time-other-amount"
+  );
+  const monthlyOtherAmountErrorMessage = document.querySelector(
+    ".error-message__monthly-other-amount"
+  );
+
+  oneTimeOtherAmountInput.addEventListener("blur", (e) => {
     if (!document.querySelector(".donation-amount__radio:checked").length) {
-      const attemptedValue = otherAmountInput.value;
-      const number = Number(attemptedValue);
-      if (!isNaN(number) && number) {
-        // hide error message
-        otherAmountErrorMessage.classList.add("hidden");
-      } else {
-        // display error message
-        otherAmountErrorMessage.classList.remove("hidden");
-      }
+      const attemptedValue = oneTimeOtherAmountInput.value;
+      inputValueCheck(attemptedValue, oneTimeOtherAmountErrorMessage);
     }
   });
+  monthlyOtherAmountInput.addEventListener("blur", (e) => {
+    if (!document.querySelector(".donation-amount__radio:checked").length) {
+      const attemptedValue = monthlyOtherAmountInput.value;
+      inputValueCheck(attemptedValue, monthlyOtherAmountErrorMessage);
+    }
+  });
+}
+
+function inputValueCheck(valueToCheck, messageToUpdate) {
+  const number = Number(valueToCheck);
+  // Checking if attempted value is a number and >= 2
+  if (!isNaN(number) && number && number >= 2) {
+    // hide error message
+    messageToUpdate.classList.add("hidden");
+  } else {
+    // display error message
+    messageToUpdate.classList.remove("hidden");
+  }
 }
 
 export default otherAmountInputValidation;
