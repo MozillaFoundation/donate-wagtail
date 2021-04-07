fac8ebd366842afa3ac08a95bb796902a9535c6e
Daniel Miranda <daniel@mozillafoundation.org>
2021-04-06 19:45:39 -0700

    added validation for minimum and maximum value of inputs for all localizations

--- source/js/components/validation.js
+++ source/js/components/validation.js
@@ -16,22 +16,27 @@ const monthlyOtherAmountErrorMessage = document.querySelector(
 function otherAmountInputValidation() {
   oneTimeOtherAmountInput.addEventListener("blur", (e) => {
     if (document.querySelector(".one-time-donation-amount-radio:checked")) {
-      const attemptedValue = oneTimeOtherAmountInput.value;
-      inputValueCheck(attemptedValue, oneTimeOtherAmountErrorMessage);
+      inputValueCheck(oneTimeOtherAmountInput, oneTimeOtherAmountErrorMessage);
     }
   });
   monthlyOtherAmountInput.addEventListener("blur", (e) => {
     if (document.querySelector(".monthly-donation-amount-radio:checked")) {
-      const attemptedValue = monthlyOtherAmountInput.value;
-      inputValueCheck(attemptedValue, monthlyOtherAmountErrorMessage);
+      inputValueCheck(monthlyOtherAmountInput, monthlyOtherAmountErrorMessage);
     }
   });
 }
 
-function inputValueCheck(valueToCheck, messageToUpdate) {
-  const number = parseFloat(valueToCheck);
-  // Checking if attempted value is a number and >= 2
-  if (!isNaN(number) && number && number >= 2) {
+function inputValueCheck(inputObjectToValidate, messageToUpdate) {
+  const number = parseFloat(inputObjectToValidate.value);
+  const minimumAmount = parseFloat(inputObjectToValidate.min);
+  const maximumAmount = parseFloat(inputObjectToValidate.max);
+  // Checking if attempted value is a number and greater the inputs minimum amount / less than the maximum amount
+  if (
+    !isNaN(number) &&
+    number &&
+    number >= minimumAmount &&
+    number <= maximumAmount
+  ) {
     // hide error message
     messageToUpdate.classList.add("hidden");
   } else {
