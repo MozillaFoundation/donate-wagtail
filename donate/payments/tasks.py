7dfbc15d0d714cf554d1622bec5d560196fb1e43
Daniel Miranda <manieldiranda@gmail.com>
2021-06-02 15:58:10 -0700

    Update tasks.py

--- donate/payments/tasks.py
+++ donate/payments/tasks.py
@@ -88,8 +88,7 @@ LANGUAGE_IDS = {
 
 # Acoustic receipt sending
 def process_donation_receipt(donation_data):
-    print("donation_data")
-    print(donation_data)
+
     # Creating new object by looping through mandatory receipt fields from const dictionary,
     # and updating them to equal the data being received
     message_data = {k: v for k, v in donation_data.items() if k in DONATION_RECEIPT_FIELDS}
@@ -114,7 +113,7 @@ def process_donation_receipt(donation_data):
     send_data = {
         DONATION_RECEIPT_FIELDS_MAP.get(k, k): v for k, v in message_data.items()
     }
-    
+
     # using the LANGUAGE_IDS const, we are getting the correct localized version of the email
     # based on the users locality, if there is none, default to English.
     message_id = LANGUAGE_IDS.get(LANGUAGE_IDS[message_data["locale"]], LANGUAGE_IDS["en-US"])
