057d6ff3dd592078ee857611df8397828b519891
Daniel Miranda <manieldiranda@gmail.com>
2021-06-03 13:20:50 -0700

    Message id key error fix (#1483)
    * removing recurring from donation_receipt_fields as it is not needed, and causes an error in the webhook
    
    * updated language ID logic, formatting of some single quotes
    
    * updated import of language_id array
    
    * updated importing of language_ids
    
    Co-authored-by: Daniel Miranda <daniel@mozillafoundation.org>

--- donate/settings/base.py
+++ donate/settings/base.py
@@ -8,7 +8,7 @@ from .environment import (
     root
 )
 
-from .languages import LANGUAGES
+from .languages import LANGUAGES, LANGUAGE_IDS
 
 
 class Base(object):
@@ -16,6 +16,8 @@ class Base(object):
     WAGTAIL_SITE_NAME = 'donate'
     WSGI_APPLICATION = 'donate.wsgi.application'
     LANGUAGE_CODE = 'en-US'
+    LANGUAGE_IDS = LANGUAGE_IDS
+    DEFAULT_LANGUAGE_ID = LANGUAGE_IDS[LANGUAGE_CODE]
     TIME_ZONE = 'UTC'
     USE_I18N = True
     USE_L10N = True
