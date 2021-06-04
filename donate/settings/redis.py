18b62afabefb4615c6aba5e9c56336dda45d4c42
Tom Usher <tom@tomusher.com>
2021-06-04 09:17:00 +0100

    Fallback to main redis instance if a queue instance isn't available

--- donate/settings/redis.py
+++ donate/settings/redis.py
@@ -3,11 +3,12 @@ from .environment import env
 
 class Redis(object):
     REDIS_URL = env('REDIS_URL')
+    REDIS_QUEUE_URL = env('REDIS_QUEUE_URL', default=REDIS_URL)
 
     CACHES = {
         'default': {
             'BACKEND': 'django_redis.cache.RedisCache',
-            'LOCATION': env('REDIS_URL'),
+            'LOCATION': REDIS_URL,
             'OPTIONS': {
                 'SOCKET_TIMEOUT': 120,
                 'SOCKET_CONNECT_TIMEOUT': 30,
@@ -19,12 +20,12 @@ class Redis(object):
 
     RQ_QUEUES = {
         'default': {
-            'URL': env('REDIS_QUEUE_URL'),
+            'URL': REDIS_QUEUE_URL,
             'DEFAULT_TIMEOUT': 500
         },
         # Must be a separate queue as it's limited to one item at a time
         'wagtail_localize_pontoon.sync': {
-            'URL': env('REDIS_QUEUE_URL'),
+            'URL': REDIS_QUEUE_URL,
             'DEFAULT_TIMEOUT': 500
         }
     }
