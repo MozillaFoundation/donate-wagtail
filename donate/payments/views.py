ca80b98ac8e7c571fa626af72c71cefa0bd0b12e
Daniel Miranda <manieldiranda@gmail.com>
2022-06-27 18:23:43 -0700

    implementation of datalayer, just need to update the gift aid section

--- donate/payments/views.py
+++ donate/payments/views.py
@@ -17,7 +17,7 @@ from mailchimp_marketing.api_client import ApiClientError
 from dateutil.relativedelta import relativedelta
 from wagtail.core.models import Page
 
-from donate.core.utils import queue_ga_event
+from donate.core.utils import queue_ga_event, queue_data_layer_event
 from . import constants, gateway
 from .exceptions import InvalidAddress
 from .forms import (
@@ -102,6 +102,35 @@ class BraintreePaymentMixin:
             }
         ])
         queue_ga_event(self.request, ['ecommerce:send'])
+
+    def queue_datalayer_event(
+        self, id, currency, amount, frequency, payment_method="credit card", gift_aid="yes"
+    ):
+        queue_data_layer_event(
+            self.request,
+            [
+                {
+                    "event": "purchase",
+                    "ecommerce": {
+                        "currency": currency,
+                        "value": str(amount),
+                        "transaction_id": id,
+                        "items": [
+                            {
+                                "item_name": frequency,
+                                "currency": currency,
+                                "index": 0,
+                                "item_brand": payment_method,
+                                "item_category": "donation",
+                                "item_variant": f"gift aid: {gift_aid}",
+                                "price": str(amount),
+                                "quantity": 1,
+                            }
+                        ],
+                    },
+                }
+            ],
+        )
 
 
 class CardPaymentView(BraintreePaymentMixin, FormView):
@@ -279,6 +308,13 @@ class CardPaymentView(BraintreePaymentMixin, FormView):
                 name='Card Donation',
                 category='one-time'
             )
+            self.queue_datalayer_event(
+                id=result.transaction.id,
+                currency=self.currency,
+                amount=form.cleaned_data['amount'],
+                frequency="single donation",
+                payment_method='credit card'
+            )
             queue_ga_event(self.request, ['send', 'event', {
                     'eventCategory': 'Donation',
                     'eventAction': 'Card',
@@ -335,6 +371,13 @@ class CardPaymentView(BraintreePaymentMixin, FormView):
                     'eventLabel': 'Monthly',
                 }
             ])
+            self.queue_datalayer_event(
+                id=result.subscription.id,
+                currency=self.currency,
+                amount=form.cleaned_data['amount'],
+                frequency="regular donation",
+                payment_method='credit card'
+            )
             # Bypass self.handle_successful_transaction, as this is not a transaction
             # but a subscription: we won't be posting anything to basket immediately,
             # instead relying on the webhook over in the 'process_webhook' task.
@@ -420,6 +463,13 @@ class PaypalPaymentView(BraintreePaymentMixin, FormView):
                     name='PayPal Donation',
                     category='one-time'
                 )
+                self.queue_datalayer_event(
+                    id=result.transaction.id,
+                    currency=self.currency,
+                    amount=form.cleaned_data['amount'],
+                    frequency='single donation',
+                    payment_method='paypal'
+                )
                 queue_ga_event(self.request, ['send', 'event', {
                         'eventCategory': 'Donation',
                         'eventAction': 'PayPal',
@@ -466,6 +516,13 @@ class PaypalPaymentView(BraintreePaymentMixin, FormView):
                         'eventLabel': 'Monthly',
                     }
                 ])
+                self.queue_datalayer_event(
+                    id=result.subscription.id,
+                    currency=self.currency,
+                    amount=form.cleaned_data['amount'],
+                    frequency='regular donation',
+                    payment_method='paypal'
+                )
 
         if result.is_success:
             return self.handle_successful_transaction(
