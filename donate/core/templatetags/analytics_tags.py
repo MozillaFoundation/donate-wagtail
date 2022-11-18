c95fc3e4f6bd17ee598639084adfc6d10298f55b
Daniel Miranda <manieldiranda@gmail.com>
2022-11-17 21:29:09 -0800

    comments

--- donate/core/templatetags/analytics_tags.py
+++ donate/core/templatetags/analytics_tags.py
@@ -4,6 +4,8 @@ register = template.Library()
 
 
 @register.inclusion_tag('fragments/ga_events.html', takes_context=True)
+# This tag is used to render GA or datalayer event data on the frontend,
+# so it can be picked up by JS.
 def render_ga_event_data(context):
     return {
         'events': context['request'].session.pop('ga_events', []),
