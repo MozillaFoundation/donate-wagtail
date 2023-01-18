b563de40f2aa7101470b119bbe38569534e00874
Daniel Miranda <manieldiranda@gmail.com>
2023-01-18 10:41:41 -0800

    passing tests

--- donate/core/migrations/0002_featureflags.py
+++ donate/core/migrations/0002_featureflags.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailcore', '0066_collection_management_permissions'),
         ('core', '0001_squashed_0015_remove_old_wagtail_localize'),
     ]
 
