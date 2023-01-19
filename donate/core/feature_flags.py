bcef2bd0b2516aceba5a68db3b17905dd620399c
Daniel Miranda <manieldiranda@gmail.com>
2023-01-19 13:46:14 -0800

    added new feature flag for enabling upsell view (#1725)
    * added new feature flag for enabling upsell view
    
    * linting
    
    * passing tests
    
    * tests

---
+++ donate/core/feature_flags.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from wagtail.admin.edit_handlers import FieldPanel, TabbedInterface, ObjectList
+from wagtail.contrib.settings.models import BaseSetting, register_setting
+
+
+@register_setting(icon="tick")
+class FeatureFlags(BaseSetting):
+
+    enable_upsell_view = models.BooleanField(
+        default=False,
+        verbose_name="Enable the upsell view",
+        help_text="Checking this will allow the upsell view to be displayed after one-time donations",
+    )
+
+    content_panels = [FieldPanel("enable_upsell_view")]
+
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(content_panels, heading="Feature Flags"),
+        ]
+    )
--- donate/payments/views.py
+++ donate/payments/views.py
@@ -17,6 +17,7 @@ from mailchimp_marketing.api_client import ApiClientError
 from dateutil.relativedelta import relativedelta
 from wagtail.core.models import Page
 
+from donate.core.feature_flags import FeatureFlags
 from donate.core.utils import queue_ga_event, queue_datalayer_event
 from . import constants, gateway
 from .exceptions import InvalidAddress
@@ -423,7 +424,9 @@ class CardPaymentView(BraintreePaymentMixin, FormView):
         return details
 
     def get_success_url(self):
-        if self.payment_frequency == constants.FREQUENCY_SINGLE:
+        enable_upsell_view = FeatureFlags.for_request(self.request).enable_upsell_view
+
+        if (self.payment_frequency == constants.FREQUENCY_SINGLE) and enable_upsell_view:
             return reverse('payments:card_upsell')
         else:
             return super().get_success_url()
@@ -583,7 +586,9 @@ class PaypalPaymentView(BraintreePaymentMixin, FormView):
         }
 
     def get_success_url(self):
-        if self.payment_frequency == constants.FREQUENCY_SINGLE:
+        enable_upsell_view = FeatureFlags.for_request(self.request).enable_upsell_view
+
+        if (self.payment_frequency == constants.FREQUENCY_SINGLE) and enable_upsell_view:
             return reverse('payments:paypal_upsell')
         else:
             return super().get_success_url()
